bcc6d3b945e37d75063c87e6952d31a5c4856552
orsetto <orsetto@a5277d2d-b329-0410-ba28-dfadbbb121a1>
2007-04-11 20:30:09 +0000

    ** add Image table ** cache system form image (all document in the future ** correct css bugs ** port all image to DB

--- trunk/application/default/controllers/ErroreController.php
+++ trunk/application/default/controllers/ErroreController.php
@@ -12,39 +12,69 @@ class ErroreController extends Zend_Controller_Action
 		
 		$this->view->title = "Errore";
 		$this->view->buttonText = '';
-		$this->view->actionTemplate = 'errore.tpl';
+		$this->view->actionTemplate = 'contents/errore.tpl';
 		$this->view->errore = array('Generic problem');
 		$this->getResponse()->setBody( $this->view->render('site.tpl') );
 	}
 	
 	public function privilegesAction(){
 		$this->view->title = "Errore";
-		$this->view->actionTemplate = 'errore.tpl';
+		$this->view->actionTemplate = 'contents/errore.tpl';
 		$this->view->errore = array('Non hai i privilegi necessari');
 		$this->getResponse()->setBody( $this->view->render('site.tpl') );
 	}
 	
 	public function fourhundredfourAction(){
 		$this->view->title = "Errore 404";
-		$this->view->actionTemplate = 'errore.tpl';
+		$this->view->actionTemplate = 'contents/errore.tpl';
 		$this->view->errore = array('File richiesto non trovato');
 		$this->getResponse()->setBody( $this->view->render('site.tpl') );
 	}
 	
 	public function invalidAction(){
 		$this->view->title = "Errore 404";
-		$this->view->actionTemplate = 'errore.tpl';
+		$this->view->actionTemplate = 'contents/errore.tpl';
 		$this->view->errore = array('La risorsa inserita non &egrave; valida');
 		$this->getResponse()->setBody( $this->view->render('site.tpl') );
 	}
 	
 	public function notsupportedAction(){
 		$this->view->title = "Errore 404";
-		$this->view->actionTemplate = 'errore.tpl';
+		$this->view->actionTemplate = 'contents/errore.tpl';
 		$this->view->errore = array('La risorsa inserita non &egrave; supportata ancora...');
 		$this->getResponse()->setBody( $this->view->render('site.tpl') );
 	}
 	
+	public function missingAction(){
+		$this->view->title = "Errore 404";
+		$this->view->actionTemplate = 'contents/errore.tpl';
+		$this->view->errore = array('Campo obbligatorio mancante! Riprovare inserendo tutti i campi necessari');
+		$this->getResponse()->setBody( $this->view->render('site.tpl') );
+	}
+	
+	public function toobigAction(){
+		$params = $this->_getAllParams();
+		$this->view->title = "Errore 404";
+		$this->view->actionTemplate = 'contents/errore.tpl';
+		$this->view->errore = array('La risorsa inserita &egrave; troppo grande. Dimensione massima '.$params['maxsize'].' bytes');
+		$this->getResponse()->setBody( $this->view->render('site.tpl') );
+	}
+	
+	public function existAction(){
+		$params = $this->_getAllParams();
+		$this->view->title = "Errore 404";
+		$this->view->actionTemplate = 'contents/errore.tpl';
+		$this->view->errore = array('La risorsa inserita esiste gi&agrave;');
+		$this->getResponse()->setBody( $this->view->render('site.tpl') );
+	}
+	
+	
+	public function notavaibleAction(){
+		$this->view->title = "Errore 404";
+		$this->view->actionTemplate = 'contents/errore.tpl';
+		$this->view->errore = array('Il sistema non &egrave; disponibile in questo momento. riprovare pi&ugrave; tardi');
+		$this->getResponse()->setBody( $this->view->render('site.tpl') );
+	}
 
 	public function noRouteAction()
 	{
--- trunk/application/default/views/scout/templates/forms/_loginForm.tpl
+++ trunk/application/default/views/scout/templates/forms/_loginForm.tpl
@@ -1,25 +1,34 @@
-<div id="login">
-	<h1><a href="http://campospe.lanzanoven.net/">Scout</a></h1>
-	<form name="loginform" id="loginform" action="/login/in/"	method="post">
-		<p>
-			<label>Mail:<br /><input type="text" name="mail" id="mail" value="" size="20" tabindex="1" /></label>
-		</p>
-		<p>
-			<label>Password:<br /> <input type="password" name="password" id="password" value="" size="20" tabindex="2" /></label>
-		</p>
-		<p>
-			<label><input name="rememberme" type="checkbox" id="rememberme" value="forever" tabindex="3" /> Remember me</label>
-		</p>
-		<p class="submit">
-			<input type="submit" name="submit" id="submit" value="{$buttonText} &raquo;" tabindex="4" />
-		</p>	
-	</form>
-	<ul>
-		<li>
-			<a href="/" title="Are you lost?"> &laquo; Back to home</a>
-		</li>
-		<li>
-			<a href="/login/lost/" title="Password Lost and Found">Lost your password?</a>
-		</li>
-	</ul>
+<div id="contents" class="login">
+
+		<div class="testo">
+			<h3>Autenticazione</h3>
+			Per accedere a determinate aree del sistema, &egrave; necessario essere autorizzati. 
+			Se necessiti di un account contattaci <a href="/contact/">qui</a>, verrai ricontattato al pi&uacute; presto. 
+			Se invece possiedi un'account valido e hai problemi, scrivi nella ML di riferimento. 
+			Per qualunque altro motivo <a href="/contact/">contattaci</a> risponderemo il prima possibile.
+		</div>
+
+		<form class="form" name="loginform" action="/login/in/" method="post">
+			<p>
+				<label>Mail:</label>
+			</p>
+			<p>
+				<input type="text" name="mail" id="mail" value="" size="20" tabindex="1" />
+			</p>
+			<p>
+				<label>Password:</label>
+			</p>
+			<p>
+				<input type="password" name="password" id="password" value="" size="20" tabindex="2" />
+			</p>
+			<p class="submit">
+				<input type="submit" name="submit" id="submit" value="{$buttonText} &raquo;" tabindex="3" />
+			</p>	
+		</form>
+		
+		<ul>
+			<li><a href="/" title="Are you lost?"> &laquo; Back to home</a></li>
+			<li><a href="/login/lost/" title="Password Lost and Found">Lost your password?</a></li>
+		</ul>
+	
 </div>
