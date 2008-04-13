d0f79460592711d70059ceb8576f499b2e9383ee
orsetto@gmail.com <orsetto@gmail.com@a5277d2d-b329-0410-ba28-dfadbbb121a1>
2008-04-13 14:53:26 +0000

    ** startup unit test

---
+++ tests/library/Sigma/ProvaTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Library_Sigma_ProvaTest extends PHPUnit_Framework_TestCase
+{
+	/**
+     * Ensures that the Singleton pattern is implemented properly
+     *
+     * @return void
+     */
+    public function testProva()
+    {
+    	echo "\n LibrarySigma\t=>\tTestProva \n";
+    	
+        $this->assertTrue(true);
+    }
+    
+}
+
+
+?>
